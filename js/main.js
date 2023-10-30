0f866e106d2d8ab4a639d7818097591d64990bd6
kryptomind11 <fkepnang@uw.edu>
2023-10-29 23:13:48 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+//adding click event
+icon.addEventListener('click', responsive_control);
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+  function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+};
+}
